9e9c549de8e8cad1d31c3a674be10af6fc33b002
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-23 08:35:07 +0100

    Chart Service

---
+++ src/main/java/com/codewithudo/quidaxchartdata/service/ChartService.java
@@ -0,0 +1,35 @@
+package com.codewithudo.quidaxchartdata.service;
+
+import com.codewithudo.quidaxchartdata.dto.Trade;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ChartService {
+
+    private static final String QUIDAX_API_BASE_URL = "https://app.quidax.com/api/v1/markets/";
+    private final RestTemplate restTemplate;
+
+    public ChartService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public List<Trade> getRecentTrades(String market) {
+        String url = QUIDAX_API_BASE_URL + market + "/trades";
+
+        // We use ParameterizedTypeReference because the API returns a list of trades (List<Trade>).
+        ResponseEntity<List<Trade>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        return response.getBody();
+    }
+}
